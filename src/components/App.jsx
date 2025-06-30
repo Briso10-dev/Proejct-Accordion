bec371179fdb037061bba51ec30e0b9fddeca0ae
Briso10-dev <kouambrice10@gmail.com>
2025-06-30 19:27:03 +0200

    Project setup to start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
+import Accordion from "./Accordion";
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
 
 function App() {
-
   return (
-    <>
-      <p className="text-8xl text-center text-blue-400">
-        Hello World!!!
-      </p>
-    </>
-  )
+    <div className="flex justify-center mt-24">
+      <Accordion />
+    </div>
+  );
 }
 
-export default App
+export default App;
