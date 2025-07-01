b01f6b95e309589904ff6e8df4056bfc98e31024
Briso10-dev <kouambrice10@gmail.com>
2025-07-01 07:29:50 +0200

    Providing key to each accordion item

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,7 +4,7 @@ function Accordion({ data }) {
   return (
     <div className="w-[70rem] flex flex-col gap-6">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} />
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title}/>
       ))}
     </div>
   );
