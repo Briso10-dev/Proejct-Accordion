c08fb72d6c949902a9031221e13a63e7fc95ff2d
Briso10-dev <kouambrice10@gmail.com>
2025-07-01 07:42:53 +0200

    Condiditional rendering of style with state

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -9,7 +9,9 @@ const  [isOpen, setIsOpen] = useState(false); //definition of state
 
   return (
     <div onClick={handleToggle}
-        className="shadow-[0_0_30px_rgba(0,0,0,0.1)] p-5 pr-12 cursor-pointer border-t-[4px] border-b-[4px] transition-all grid grid-cols-[auto_1fr_auto] gap-x-6 gap-y-8 items-center">
+        className={`shadow-[0_0_30px_rgba(0,0,0,0.1)] p-5 pr-12 cursor-pointer border-t-[4px] border-b-[4px] transition-all grid grid-cols-[auto_1fr_auto] gap-x-6 gap-y-8 items-center ${
+        isOpen ? "border-t-[#087f5b] text-[#087f5b]" : "border-t-white border-b-white"
+        }`}>
       {/* Number */}
       <p className="text-3xl font-medium text-[#ced4da]">{num < 9 ? `0${num +1}` : num + 1} </p>
 
