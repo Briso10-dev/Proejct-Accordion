527d01838ca874cd1428697030af66644bb12f67
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 17:36:30 +0200

    toogle one Accordion item - other close

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 
 function AccordionItem({ num, title, curOpen, onOpen, children }) {
 const isOpen = num === curOpen
 
     function handleToggle(){
-        onOpen(num )
+        onOpen(isOpen ? null : num);
     }
 
   return (
